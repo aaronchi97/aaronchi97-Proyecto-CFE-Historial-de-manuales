8cfd3cf1a655414363757b56a44b9db0d75c55cd
Geovanny Ac <seiya_90@outlook.es>
2024-01-31 00:24:14 -0600

    commit función subida de archivos

---
+++ vista/SINASU_AGENCIAS/subir_archivos.php
@@ -0,0 +1,115 @@
+<?php
+
+session_start();
+//si el nombre y apellido estan vacios entonces redirigelos a la pagina de login.php
+//esto hace que si quieres colocar el link que te arroja el navegador al iniciar sesion
+//lo compias y lo pegas desde el inicio entonces no te dejara, hasta que pongas un usuario valido
+if (empty($_SESSION['nombre-sinasu']) and empty($_SESSION['apellido-sinasu'])) {
+  header("location:../login/login_sinasu.php");
+}
+
+?>
+<style>
+  ul li:nth-child(1) .activo {
+    background: #9889fe !important;
+  }
+</style>
+<!-- primero se carga el topbar -->
+<?php require('./../layout/topbar_sinasu.php'); ?>
+<!-- luego se carga el sidebar -->
+<?php require('./../layout/sidebar_sinasu.php'); ?>
+
+
+<!-- inicio del contenido principal -->
+
+
+<link rel="stylesheet" href="../SINASU/estilosinasu.css">
+
+<body class="cuerpo_subir">
+  <div class="page-content">
+    <a href="agencia1.php" class="btn btn-danger btn-rounded mb-3 otro"><i class="fa-regular fa-circle-left"></i> &nbsp;
+      ATRAS</a>
+    <div class="subir">
+      <div class="drop-area">
+        <h2>Arrastra y suelta el documento PDF</h2>
+        <span>O</i>
+        </span>
+        <button>Selecciona tus archivos</button>
+        <input type="file" name="" id="input-file" hidden multipart>
+      </div>
+      <div id="preview">
+      </div>
+    </div>
+
+  </div>
+</body>
+<script src="upload.js"></script>
+
+
+
+<!-- <div class="page-content">
+  <h1 class="titulo_subir">Subir Archivos</h1>
+  <a href="agencia1.php" class="btn btn-danger btn-rounded mb-3 otro"><i class="fa-regular fa-circle-left"></i> &nbsp;
+    ATRAS</a>
+  <section class="seccion_subir">
+    <article>
+      <div>
+        <form class="form_subir" action="upload.php" method="post" enctype="multipart/form-data" id="uploadForm">
+          <label for="pdfFile">Seleccionar archivo PDF:</label>
+          <input type="file" name="pdfFile" id="pdfFile" accept=".pdf" onchange="previewFile()">
+          <br>
+          <img src="" alt="Vista previa" id="previewImg" style="display:none;">
+          <br>
+          <div id="drop-area" ondrop="dropHandler(event);" ondragover="dragOverHandler(event)">
+            <p>O arrastra y suelta tu archivo PDF aquí</p>
+            <p id="fileNameDisplay"></p>
+          </div>
+          <br>
+          <input type="submit" value="Subir PDF">
+        </form>
+      </div>
+    </article>
+  </section>
+</div>
+
+<script>
+  function dropHandler(event) {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+
+    // Verifica si hay archivos
+    if (files.length > 0) {
+      // Muestra el nombre del archivo debajo del área de arrastre
+      const fileNameDisplay = document.getElementById('fileNameDisplay');
+      fileNameDisplay.innerHTML = 'Nombre del archivo: ' + files[0].name;
+    }
+  }
+
+  function dragOverHandler(event) {
+    event.preventDefault();
+  }
+
+  function previewFile() {
+    const preview = document.getElementById('previewImg');
+    const file = document.getElementById('pdfFile').files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+      preview.src = reader.result;
+      preview.style.display = 'block';
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = '';
+      preview.style.display = 'none';
+    }
+  }
+
+</script> -->
+
+
+
+<!-- por ultimo se carga el footer -->
+<?php require('./../layout/footer_sinasu.php'); ?>
---
+++ vista/SINASU_AGENCIAS/upload.js
@@ -0,0 +1,194 @@
+const dropArea = document.querySelector(".drop-area");
+const dragText = dropArea.querySelector("h2");
+const button = dropArea.querySelector("button");
+const input = dropArea.querySelector("#input-file");
+let files;
+
+button.addEventListener("click", (e) => {
+  input.setAttribute("multiple", "multiple");
+  input.click();
+});
+
+input.addEventListener("change", (e) => {
+  files = input.files;
+  dropArea.classList.add("active");
+  showFiles(files);
+  dropArea.classList.remove("active");
+});
+
+dropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropArea.classList.add("active");
+  dragText.textContent = "Suelta para subir los archivos";
+});
+
+dropArea.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  dropArea.classList.remove("active");
+  dragText.textContent = "Arrastra y suelta el documento PDF";
+});
+
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  files = e.dataTransfer.files;
+  showFiles(files);
+  dropArea.classList.remove("active");
+  dragText.textContent = "Arrastra y suelta el documento PDF";
+});
+
+function showFiles(files) {
+  if (files && files.length !== undefined) {
+    // Verifica si files no es undefined
+    if (files.length === 0) {
+      console.log("No hay archivos para mostrar");
+    } else {
+      for (const file of files) {
+        processFile(file);
+      }
+    }
+  } else {
+    console.log("No se proporcionaron archivos");
+  }
+}
+
+function processFile(file) {
+  const docType = file.type;
+  const validExtensions = ["application/pdf"];
+
+  if (validExtensions.includes(docType)) {
+    //archivo valido
+    const fileReader = new FileReader();
+    const id = `file-${Math.random().toString(32).substring(7)}`;
+
+    fileReader.addEventListener("load", (e) => {
+      const fileUrl = fileReader.result;
+      const documentpdf = `
+        <div id="${id}" class="file-container">
+        <i class="fa-regular fa-file-pdf" style="font-size: 20px;"></i>
+          <div class="status">
+            <span> <a href="uploads/${file.name}" target="_blank">${file.name}</a></span>
+            <span class="status-text">
+              Loading...
+            </span>
+            <span><a href="#" onclick="eliminarArchivo('${id}', '${file.name}')"><i class="fa-solid fa-circle-xmark"></i> Eliminar</a></span>
+          </div>
+        </div>
+        `;
+
+      const html = document.querySelector("#preview").innerHTML;
+      document.querySelector("#preview").innerHTML = documentpdf + html;
+    });
+
+    fileReader.readAsDataURL(file);
+    uploadFile(file, id);
+  } else {
+    //no es un archivo válido
+    alert("No es un archivo válido");
+  }
+}
+
+async function uploadFile(file, id) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch("upload.php", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al subir el archivo");
+    }
+
+    const responseText = await response.text();
+    console.log(responseText); // Muestra la respuesta del servidor en la consola
+
+    let responseData;
+    try {
+      responseData = JSON.parse(responseText); // Intenta analizar la respuesta como JSON
+    } catch (error) {
+      console.error("Error al analizar la respuesta JSON:", error);
+    }
+
+    if (responseData && responseData.error) {
+      // Manejar el error de manera adecuada
+      console.error("Error en la carga del archivo:", responseData.error);
+      document.querySelector(
+        `#${id} .status-text`
+      ).innerHTML = `<span class="failure">${responseData.error}</span>`;
+    } else {
+      // Manejar la respuesta exitosa de manera adecuada
+      console.log("Archivo subido correctamente");
+      document.querySelector(
+        `#${id} .status-text`
+      ).innerHTML = `<span class="success">Archivo subido correctamente...</span>`;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    // Aquí puedes manejar el error, como mostrar un mensaje al usuario
+    document.querySelector(
+      `#${id} .status-text`
+    ).innerHTML = `<span class="failure">El archivo no pudo subirse...</span>`;
+  }
+}
+
+function eliminarArchivo(id, fileName, id_documento) {
+  if (confirm("¿Estás seguro de que quieres eliminar este archivo?")) {
+    // Imprimir el JSON que se enviará en la solicitud
+    console.log(
+      JSON.stringify({
+        id: id,
+        fileName: fileName,
+        idDocumento: id_documento,
+      })
+    );
+
+    // Realizar la eliminación del archivo
+    fetch("eliminar_archivo.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+        fileName: fileName,
+        idDocumento: id_documento,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al eliminar el archivo");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // Aquí puedes actualizar la interfaz de usuario para reflejar la eliminación del archivo
+        document.getElementById(id).remove(); // Eliminar el contenedor del archivo del DOM
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Aquí puedes mostrar un mensaje de error al usuario
+      });
+  }
+}
+
+// function uploadFile(file) {
+//   const formData = new FormData();
+//   formData.append("file", file);
+
+// try {
+//   const response = await fetch('http:localhost:3000/upload', {
+//     method: "POST",
+//     body: formData,
+//   });
+
+//   const responseText = await response.text();
+//   console.log(responseText);
+
+//   document.querySelector(`#${id} .status-text`).innerHTML = `<span class="success">Archivo subido correctamente...</span>`;
+// } catch (error) {
+//   document.querySelector(`#${id} .status-text`).innerHTML = `<span class="failure">El archivo no pudo subirse...</span>`;
+// }
+// }
